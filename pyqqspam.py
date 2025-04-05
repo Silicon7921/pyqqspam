3998cb44cc9a2254fc2062ada03e5b8c328328b0
CorePomelo <pomeloalt@foxmail.com>
2025-04-05 12:58:55 +0800

    recontruct code

--- pyqqspam.py
+++ pyqqspam.py
@@ -1,34 +1,72 @@
 from pynput import keyboard
 import pyautogui
-import sys
 import time
 import pyperclip
-listener=None
+import threading
 
-print("pyqqspam")
-print("if <F4> is pressed spam will begin. <ctrl>+c to quit.")
-spam_num=int(input("how many times do you want to spam? : "))
-pyperclip.copy(input("input spam message: "))
-interval=float(input("input spam delay (second): "))
+f4_pressed = False
 
-def F4_down():
-    print("F4 is pressed, start spamming.")
-    for _ in range(spam_num):
-            pyautogui.hotkey("ctrl","v")
-            time.sleep(0.001)
-            pyautogui.press("enter")
-            print("msg number",_,"sent, total count is",spam_num,"sleeping...")
-            time.sleep(interval)
+class HotkeyManager:
+    def __init__(self):
+        self.ctrl_pressed = False
+        self.listener = None
+        self.running = True
+        print("init.")
 
-def ctrlc_shutdown():
-    global listener
-    listener.stop()
-    print("ctrl+c, quit.")
-    sys.exit(0)
-    return False
+    def on_press(self, key):
+        global f4_pressed
+        try:
+            if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+                self.ctrl_pressed = True
+            elif key == keyboard.KeyCode.from_char('c') and self.ctrl_pressed:
+                self.terminate()
+            if key == keyboard.Key.f4:
+                f4_pressed = True
+        except AttributeError:
+            pass
 
-with keyboard.GlobalHotKeys({"<F4>":F4_down,"<ctrl>+c":ctrlc_shutdown}) as listener_obj:
-    listener=listener_obj
-    time.sleep(1)
-    print("ready to spam. listening...")
-    listener.join()
+    def on_release(self, key):
+        global f4_pressed
+        if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+            self.ctrl_pressed = False
+        if key == keyboard.Key.f4:
+            f4_pressed = False
+
+    def start_listener(self):
+        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        self.listener.start()
+        print("listener started.")
+
+    def terminate(self):
+        self.running = False
+        if self.listener is not None:
+            self.listener.stop()
+        print("Exiting...")
+
+if __name__ == "__main__":
+    manager = HotkeyManager()
+    print("pyqqspam")
+    print("if <F4> is pressed spam will begin. <ctrl>+c to quit.")
+    spam_num = int(input("how many times do you want to spam? : "))
+    pyperclip.copy(input("input spam message: "))
+    interval = float(input("input spam delay (second): "))
+
+    def spam_keys():
+        while manager.running:
+            if f4_pressed:
+                for _ in range(spam_num):
+                    pyautogui.hotkey("ctrl", "v")
+                    time.sleep(0.001)
+                    pyautogui.press("enter")
+                    print(f"msg {_+1} of {spam_num} sent, sleeping...")
+                    time.sleep(interval)
+
+    spam_thread = threading.Thread(target=spam_keys, daemon=True)
+    manager.start_listener()
+    spam_thread.start()
+
+    try:
+        while manager.running:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        manager.terminate()
